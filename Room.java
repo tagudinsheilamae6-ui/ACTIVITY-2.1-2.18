d6a26b3d9179eaa50e6112538caf27bdd581c8ea
tagudinsheilamae6-ui <tagudinsheilamae6@gmail.com>
2026-09-25 20:52:40 +0800

    Activity 2.9 Room

---
+++ Room.java
@@ -0,0 +1,13 @@
+//Sheila Tagudin BSIT NETSEC 1-1
+public class Room {
+    public static void main(String[] args) {
+        int length = 20;
+        int width = 15;
+
+        int floorSpace = length * width;
+
+        System.out.println("Length: " + length + " feet");
+        System.out.println("Width: " + width + " feet");
+        System.out.println("The floor space is " + floorSpace + " square feet.");
+    }
+}
