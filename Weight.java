b85dec964c127777d44b115dc17e607d04766167
tagudinsheilamae6-ui <tagudinsheilamae6@gmail.com>
2026-09-25 20:48:40 +0800

    Activity 2.8 Weight

---
+++ Weight.java
@@ -0,0 +1,10 @@
+//Sheila Tagudin BSIT NETSEC 1-1
+public class Weight {
+    public static void main(String[] args) {
+        double pounds = 10;
+        double kilograms = pounds * 0.454;
+
+        System.out.println("Weight in pounds: " + pounds);
+        System.out.println("Weight in kilograms: " + kilograms);
+    }
+}
