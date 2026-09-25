fa7934a0466d758d8da76846b36c4dc76c907779
tagudinsheilamae6-ui <tagudinsheilamae6@gmail.com>
2026-09-25 20:44:54 +0800

    Activity 2.7 Circlee

---
+++ Circlee.java
@@ -0,0 +1,10 @@
+//Sheila Tagudin BSIT NETSEC 1-1
+public class Circlee {
+    public static void main(String[] args) {
+        int radius = 2;
+
+        double area = radius * radius * 3.142;
+
+        System.out.println("The area of the circle is: " + area);
+    }
+}
