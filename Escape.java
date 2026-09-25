3eb9d54e077ac24cfc5807b68019c59bebee552d
tagudinsheilamae6-ui <tagudinsheilamae6@gmail.com>
2026-09-25 21:24:01 +0800

    Activity 2.15 Escape

---
+++ Escape.java
@@ -0,0 +1,7 @@
+// Sheila Mae Tagudin
+public class Escape {
+    public static void main(String[] args) {
+        System.out.println("\"First Name\"\t\"Last Name\"\tStudent Number\tPhone Number");
+        System.out.println("Sheila        \tTagudin          \t26-015      \t09623738268");
+    }
+}
