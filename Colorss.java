4884ca278cf652ee2592af440d6c4257636ca575
tagudinsheilamae6-ui <tagudinsheilamae6@gmail.com>
2026-09-25 20:33:00 +0800

    Activity 2.3 Colorss

---
+++ Colorss.java
@@ -0,0 +1,18 @@
+// Shiela Tagudin BSIT NETSEC 1-1
+public class Colorss {
+    public static void main(String[] args) {
+
+        // My Favorite Colors
+        System.out.println("My Favorite Colors are:");
+
+        String color1 = "yellow";
+        String color2 = "green";
+        String color3 = "red";
+        String color4 = "blue";
+
+        System.out.println(color1);
+        System.out.println(color2);
+        System.out.println(color3);
+        System.out.println(color4);
+    }
+}
