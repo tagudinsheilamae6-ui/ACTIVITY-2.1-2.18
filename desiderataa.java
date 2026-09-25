e196954f9431ec65d4027c4ebff7b26a07bfbef6
tagudinsheilamae6-ui <tagudinsheilamae6@gmail.com>
2026-09-25 20:24:14 +0800

    Activity 2.1 desiderataa

---
+++ desiderataa.java
@@ -0,0 +1,15 @@
+// Shiela Tagudin BSIT NETSEC 1-1
+public class Desiderataa {
+    public static void main(String[] args) {
+
+        System.out.println("Go placidly amid the noise and haste,");
+        System.out.println("and remember what peace there may be in silence.");
+        System.out.println("As far as possible, without surrender,");
+        System.out.println("be on good terms with all persons.");
+        System.out.println("Speak your truth quietly and clearly;");
+        System.out.println("and listen to others, even to the dull and the ignorant,");
+        System.out.println("they too have their story.");
+        System.out.println("Avoid loud and aggressive person.");
+
+    }
+}
