eeec402cca6bba1d899a63723db348d1d2280580
tagudinsheilamae6-ui <tagudinsheilamae6@gmail.com>
2026-09-25 21:17:13 +0800

    Activity 2.12 Averagee

---
+++ Averagee.java
@@ -0,0 +1,17 @@
+// Sheila Mae Tagudin
+public class Averagee {
+
+    public static void main(String[] args) {
+
+        int number1 = 10;
+        int number2 = 20;
+        int number3 = 45;
+
+        int average = (number1 + number2 + number3) / 3;
+
+        System.out.println("number 1 = " + number1);
+        System.out.println("number 2 = " + number2);
+        System.out.println("number 3 = " + number3);
+        System.out.println("average = " + average);
+    }
+}
