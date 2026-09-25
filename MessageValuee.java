5a1595ca283c1081454bacfedf529a5250a88374
tagudinsheilamae6-ui <tagudinsheilamae6@gmail.com>
2026-09-25 20:40:58 +0800

    Activity 2.6 MessageValuee

---
+++ MessageValuee.java
@@ -0,0 +1,10 @@
+// Shiela Tagudin BSIT NETSEC 1-1
+public class MessageValuee {
+    public static void main(String[] args) {
+        String message = "The value of pie is:";
+        double value = 3.1415;
+
+        System.out.println(message);
+        System.out.println(value);
+    }
+}
