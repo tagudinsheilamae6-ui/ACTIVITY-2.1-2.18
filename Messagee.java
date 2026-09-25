ae0da7de5016118842ee07a020a164e53a11b87b
tagudinsheilamae6-ui <tagudinsheilamae6@gmail.com>
2026-09-25 20:38:35 +0800

    Activity 2.5 Messagee

---
+++ Messagee.java
@@ -0,0 +1,12 @@
+// Shiela Tagudin BSIT NETSEC 1-1
+public class Messagee {
+    public static void main(String[] args) {
+        String message1 = "I love my course";
+        String message2 = "I love Java programming";
+        String message3 = "I want to be a Java programmer";
+
+        System.out.println(message1);
+        System.out.println(message2);
+        System.out.println(message3);
+    }
+}
