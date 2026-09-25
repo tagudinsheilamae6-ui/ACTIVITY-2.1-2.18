4216110015f1364865916dc1e4b5c86674235c8a
tagudinsheilamae6-ui <tagudinsheilamae6@gmail.com>
2026-09-25 20:35:52 +0800

    Activity 2.4 SequentialNumberss

---
+++ SequentialNumberss.java
@@ -0,0 +1,13 @@
+// Shiela Tagudin BSIT NETSEC 1-1
+public class SequentialNumberss {
+    public static void main(String[] args) {
+        int sequence1 = 123;
+        int sequence2 = 456;
+        int sequence3 = 789;
+
+        System.out.println("Sequential Numbers");
+        System.out.println(sequence1);
+        System.out.println(sequence2);
+        System.out.println(sequence3);
+    }
+}
