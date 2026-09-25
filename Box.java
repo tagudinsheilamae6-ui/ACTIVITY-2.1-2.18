939266831aa45ebea8abe01023024788e002d973
tagudinsheilamae6-ui <tagudinsheilamae6@gmail.com>
2026-09-25 21:27:32 +0800

    Activity 2.17 Box

---
+++ Box.java
@@ -0,0 +1,13 @@
+//Sheila Mae Tagudin
+public class Box {
+    public static void main(String[] args) {
+
+        System.out.println("********* \t   *");
+        System.out.println("*       * \t  * *");
+        System.out.println("*       * \t *   *");
+        System.out.println("*       * \t*     *");
+        System.out.println("*       * \t *   *");
+        System.out.println("*       * \t  * *");
+        System.out.println("********* \t   *");
+    }
+}
